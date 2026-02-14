0ba8db904bc24ba012dc12abd945b82cb89433fe
genrex-code <jairo2030antonio007@gmail.com>
2026-02-14 02:02:04 -0600

    carpeta scrips completa :D

---
+++ proyecto/scripts/run_pipeline.py
@@ -0,0 +1,71 @@
+"""
+run_pipeline.py
+pipeline principal de lsistema de analisis de cleitnes HPE
+Equipo Devcode
+
+"""
+config = {
+    "data_path": "data/synthetic/",
+    "output_path": "results/",
+    "modo": "test"
+}
+
+import time
+
+#modulos del proyecto ( sujeto a cambios)
+from modules.recolector import cargar_datos
+from modules.extractor import extraer_senales
+from modules.scoring import calcular_score
+from modules.clasificador import clasificar_clientes
+from modules.recomendador import recomendar_hpe
+from modules.salida import guardar_resultados
+
+def log_paso(nombre):
+    """Imprime separador bonito para debug"""
+    print("\n" + "="*50)
+    print(f"--> Ejecutando módulo: {nombre}")
+    print("="*50)
+
+
+def run_pipeline(config):
+    """
+    ejecucion de todo el flujo del sistema IA
+    config = diccionario con rutas y parametros
+    """
+    inicio_total = time.time()
+    try:
+        log_paso("recolector")
+        clientes,eventos,historial = cargar_datos(config)
+        
+        # ---------------------------
+        log_paso("2. Extractor de señales")
+        features = extraer_senales(clientes, eventos, historial)
+
+        # ---------------------------
+        log_paso("3. Scoring")
+        scores = calcular_score(features)
+
+        # ---------------------------
+        log_paso("4. Clasificador")
+        segmentos = clasificar_clientes(scores)
+
+        # ---------------------------
+        log_paso("5. Recomendador HPE")
+        recomendaciones = recomendar_hpe(segmentos)
+
+        # ---------------------------
+        log_paso("6. Salida")
+        guardar_resultados(recomendaciones, config)
+    except Exception as e:
+        print(f"Error en ejecución del pipeline: {e}")
+        raise
+    fin_total = time.time()
+    print(f"Pipeline completado en {fin_total - inicio_total:.2f} segundos")
+
+if __name__ == "__main__":
+    config = {
+        "data_path": "data/synthetic/",
+        "output_path": "results/",
+        "modo": "test"
+    }
+    run_pipeline(config)
